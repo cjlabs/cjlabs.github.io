6f46a2384f374fc78fa038102bfa4bfff8a087db
puterakahfi <puterakahfi@gmail.com>
2019-02-03 21:21:23 +0700

    init nuxtjs

---
+++ components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-navbar toggleable="md" type="light" class="container">
+    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+    <b-navbar-brand href="/"><strong>Cjlabs</strong></b-navbar-brand>
+
+    <b-navbar-nav>
+      <b-nav-item href="#">Codeigniter Class</b-nav-item>
+      <b-nav-item href="#">Laravel Class</b-nav-item>
+      <b-nav-item href="#">Symfony Class</b-nav-item>
+      <b-nav-item href="#">Vuejs Class</b-nav-item>
+      <b-nav-item href="#">React Class</b-nav-item>
+    </b-navbar-nav>
+
+    <b-collapse is-nav id="nav_collapse">
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="#">Daftar</b-nav-item>
+        <b-nav-item href="/daftar">Login</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<style>
+.container {
+  padding: 25px 0px !important;
+  min-height: 10vh !important;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,79 @@
+<template>
+  <section>
+
+  <Navbar></Navbar>
+<section class="container">
+
+    <div class="col-md-6">
+      <img class="img-fluid" src="~/assets/code.jpg">
+    </div>
+
+    <div class="col-md-6" >
+      <h1 class="title">
+       CJLABS Learning Center
+      </h1>
+      <p class="subtitle">
+        Leverage your skill to next level
+      </p>
+
+      <div class="links">
+        <a
+          href="daftar"
+          class="btn btn-primary"
+        >
+          Daftar
+        </a>
+        <a
+          href="/login"
+          class="btn btn-secondary"
+        >
+          Login
+        </a>
+      </div>
+    </div>
+</section>
+  </section>
+</template>
+
+<script>
+import Navbar from '~/components/Navbar.vue'
+
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 800;
+  font-size: 2.5rem;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 35px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
