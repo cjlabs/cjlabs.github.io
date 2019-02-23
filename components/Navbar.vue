b68c54a95d0050cf4bcaf4e6c65792aa26ec97b6
puterakahfi <puterakahfi@gmail.com>
2019-02-23 17:45:03 +0700

    add sample course page

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,10 +1,10 @@
 <template>
-  <b-navbar toggleable="md" type="light" class="container">
+  <b-navbar toggleable="md" type="light" class="container sticky-top">
     <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
 
     <b-navbar-brand href="/">
       <strong>
-        <font-awesome-icon icon="code"/>Cjlabs
+        <font-awesome-icon icon="code"/> CJLABS
       </strong>
     </b-navbar-brand>
 
@@ -12,11 +12,10 @@
 
     <b-collapse is-nav id="nav_collapse">
     <b-navbar-nav>
-      <b-nav-item href="/detail-course">Codeigniter</b-nav-item>
-      <b-nav-item href="#">laravel</b-nav-item>
+      <b-nav-item href="/sample-course">sample-kursus</b-nav-item>
       <b-nav-item href="#">symfony</b-nav-item>
-      <b-nav-item href="#">nuxtjs</b-nav-item>
-      <b-nav-item href="#">store</b-nav-item>
+      <b-nav-item href="#">vue</b-nav-item>
+      <b-nav-item href="#">jadwal</b-nav-item>
     </b-navbar-nav>
     </b-collapse>
 
---
+++ pages/sample-course.vue
@@ -0,0 +1,94 @@
+<template>
+  <section class="container">
+    <div>
+      <b-card
+        overlay
+        img-src="https://picsum.photos/900/250/?image=3"
+        img-alt="Card Image"
+        text-variant="white"
+        title="kursus vue js"
+        sub-title="Subtitle"
+      >
+        <b-card-text>
+          <p>
+            ue (pronounced /vjuː/, like view) is a progressive framework for building user interfaces. Unlike other monolithic frameworks, Vue is designed from the ground up to be incrementally adoptable. The core library is focused on the view layer only, and is easy to pick up and integrate with other libraries or existing projects. On the other hand, Vue is also perfectly capable of powering sophisticated Single-Page Applications when used in combination with modern tooling and supporting libraries.
+            If you’d like to learn more about Vue before diving in, we created a video walking through the core principles and a sample project.
+            If you are an experienced frontend developer and want to know how Vue compares to other libraries/frameworks, check out the Comparison with Other Frameworks.
+          </p>
+        </b-card-text>
+      </b-card>
+    </div>
+  <br/>
+
+    <b-card title="Introduction" sub-title="Card subtitle">
+      <b-card-text>
+        Some quick example text to build on the
+        <em>card title</em> and make up the bulk of the card's
+        content.
+      </b-card-text>
+
+      <b-card-text>
+        <p>
+          ue (pronounced /vjuː/, like view) is a progressive framework for building user interfaces.
+          Unlike other monolithic frameworks, Vue is designed from the ground up to be incrementally
+          adoptable.
+        </p>
+      </b-card-text>
+
+      <a href="#" class="card-link">Download Materi</a>
+      <b-link href="#" class="card-link">Another link</b-link>
+    </b-card>
+    <br>
+
+    <b-card title="Setup Environment" sub-title="Setup Vue project">
+      <b-card-text>
+        Some quick example text to build on the
+        <em>card title</em> and make up the bulk of the card's
+        content.
+      </b-card-text>
+
+      <br>
+
+      <b-card-text>
+        <p>
+          ue (pronounced /vjuː/, like view) is a progressive framework for building user interfaces.
+          Unlike other monolithic frameworks, Vue is designed from the ground up to be incrementally
+          adoptable.
+        </p>
+      </b-card-text>
+
+      <a href="#" class="card-link">Download Materi</a>
+      <b-link href="#" class="card-link">Another link</b-link>
+    </b-card>
+
+  <br/>
+    <b-card title="Vue Instance" sub-title="Vue Instance">
+      <b-card-text>
+        Some quick example text to build on the
+        <em>card title</em> and make up the bulk of the card's
+        content.
+      </b-card-text>
+
+      <br>
+
+      <b-card-text>
+        <p>
+          ue (pronounced /vjuː/, like view) is a progressive framework for building user interfaces.
+          Unlike other monolithic frameworks, Vue is designed from the ground up to be incrementally
+          adoptable.
+        </p>
+      </b-card-text>
+
+      <a href="#" class="card-link">Download Materi</a>
+      <b-link href="#" class="card-link">Another link</b-link>
+    </b-card>
+
+  </section>
+</template>
+
+<script>
+export default {
+  layout: 'default',
+  components: {}
+}
+</script>
