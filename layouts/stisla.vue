fb28824a30f6ae10a034d30b59c7efac9d5150b9
puterakahfi <puterakahfi@gmail.com>
2019-03-24 11:37:34 +0700

    update

---
+++ layouts/stisla.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="main-wrapper main-wrapper-1">
+    <div class="container">
+      <Navbar></Navbar>
+      <br>
+      <br>
+      <nuxt></nuxt>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import Navbar from '~/components/Stisla/Navbar.vue'
+//import Footer from '~/components/Footer.vue'
+
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
