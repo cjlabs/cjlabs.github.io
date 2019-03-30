454342382c5da2bae19e882da0183846567e4771
puterakahfi <puterakahfi@gmail.com>
2019-03-30 11:50:39 +0700

    add series store

---
+++ store/series.js
@@ -0,0 +1,49 @@
+export const state = () => ({
+  course: {
+    title: 'Symfony API Development'
+  },
+  list: [
+    {
+      title: 'Materi 1',
+      subtitle: 'subjudul',
+      image: 'bookmarks.svg',
+      body:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod . '
+    },
+    {
+      title: 'Materi 2',
+      subtitle: 'subjudul',
+      image: 'undraw_dashboard_nklg.svg',
+      body:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod . '
+    },
+    {
+      title: 'Materi 3',
+      subtitle: 'subjudul',
+      image: 'undraw_to_do_list_a49b.svg',
+      body:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod . '
+    },
+    {
+      title: 'Materi 4 ',
+      subtitle: 'subjudul',
+      image: 'undraw_wallet_aym5.svg',
+      body:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod . '
+    },
+    {
+      title: 'Materi 5',
+      subtitle: 'subjudul',
+      image: 'undraw_calendar_dutt.svg',
+      body:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod . '
+    },
+    {
+      title: 'Materi 6',
+      subtitle: 'subjudul',
+      image: 'undraw_dashboard_nklg.svg',
+      body:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod . '
+    }
+  ]
+})
